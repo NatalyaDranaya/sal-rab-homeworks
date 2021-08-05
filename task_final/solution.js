f60c866ba242ed9b9fdcc4a737ac39f80953fcdb
Natalya Dranaya <natalyadranaya@gmail.com>
2021-08-05 22:28:57 +0300

    final task fix googs

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,8 +36,15 @@
 function sendRequest(name, phone, address, goods, sum) {
     let client = name + " " + phone;
     let addressHumanReadable = "ул. " + address.street + ", дом " + address.house + ", "+ address.entrance + " подъезд, "+ address.floor+" этаж, кв " + address.flat;
-    let data = {client, order: {address: addressHumanReadable, sum}, goods};
+    
+    let newGoods = [];
+    
+    for (let i=0; i< goods.length; i++ ){
+        let item = goods [i];
+        let newitem = {title:item.title, count:item.count};
+        newGoods [i] = newitem; 
+    }
 
-    return JSON.stringify(data);
+    let data = {client, order: {address: addressHumanReadable, sum}, goods:newGoods};
+    return JSON.stringify({data});
 }
- 
